e37a4b8dde9b597fa9d3bd0f4b05e6aa7c90b0e9
Pim van der Loos <pimvanderloos@gmail.com>
2022-07-02 19:08:28 +0200

    Get offline player async in HeadManager

--- bigdoors-spigot/spigot-core/src/main/java/nl/pim16aap2/bigdoors/spigot/managers/HeadManager.java
+++ bigdoors-spigot/spigot-core/src/main/java/nl/pim16aap2/bigdoors/spigot/managers/HeadManager.java
@@ -11,7 +11,6 @@ import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.jetbrains.annotations.Nullable;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -19,7 +18,6 @@ import java.time.Duration;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.logging.Level;
 
 /**
  * Represents a manager of player heads with the texture of a certain player.
@@ -29,6 +27,8 @@ import java.util.logging.Level;
 @Singleton @Flogger
 public final class HeadManager extends Restartable
 {
+    private static final ItemStack FALLBACK = new ItemStack(Material.PLAYER_HEAD);
+
     /**
      * Timed cache of player heads.
      * <p>
@@ -36,7 +36,7 @@ public final class HeadManager extends Restartable
      * <p>
      * Value: The player's head as item.
      */
-    private @Nullable TimedCache<UUID, Optional<ItemStack>> headMap;
+    private TimedCache<UUID, ItemStack> headMap;
     private final ConfigLoaderSpigot config;
 
     /**
@@ -52,52 +52,45 @@ public final class HeadManager extends Restartable
     {
         super(holder);
         this.config = config;
+        headMap = TimedCache.emptyCache();
     }
 
     /**
      * Requests the ItemStack of a head with the texture of the player's head. This is done asynchronously because it
      * can take quite a bit of time.
      *
-     * @param playerUUID
+     * @param uuid
      *     The {@link UUID} of the player whose head to get.
      * @param displayName
      *     The display name to give assign to the {@link ItemStack}.
      * @return The ItemStack of a head with the texture of the player's head if possible.
      */
     @SuppressWarnings("unused")
-    public CompletableFuture<Optional<ItemStack>> getPlayerHead(UUID playerUUID, String displayName)
+    public CompletableFuture<ItemStack> getPlayerHead(UUID uuid, String displayName)
     {
-        if (headMap == null)
-        {
-            log.at(Level.SEVERE).log("Trying to retrieve player head while head map is not initialized!");
-            return CompletableFuture.completedFuture(Optional.empty());
-        }
-        // Satisfy NullAway that headMap won't be null.
-        final var headMapNN = headMap;
+        final Optional<ItemStack> head = headMap.get(uuid);
+        return head.map(CompletableFuture::completedFuture)
+                   .orElseGet(() -> CompletableFuture
+                       .supplyAsync(() -> Bukkit.getOfflinePlayer(uuid))
+                       .thenApplyAsync(offlinePlayer -> headMap.put(uuid, createItemStack(offlinePlayer, displayName)))
+                       .exceptionally(t -> Util.exceptionally(t, FALLBACK)));
 
-        return CompletableFuture
-            .supplyAsync(() -> headMapNN.computeIfAbsent(playerUUID,
-                                                         (p) -> createItemStack(playerUUID, displayName)))
-            .exceptionally(Util::exceptionallyOptional);
     }
 
-    private Optional<ItemStack> createItemStack(UUID playerUUID, String displayName)
+    private ItemStack createItemStack(OfflinePlayer offlinePlayer, String displayName)
     {
-        final OfflinePlayer oPlayer = Bukkit.getOfflinePlayer(playerUUID);
         final ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
-        final @Nullable SkullMeta sMeta = (SkullMeta) skull.getItemMeta();
-        if (sMeta == null)
-            return Optional.empty();
-        sMeta.setOwningPlayer(oPlayer);
+        final SkullMeta sMeta = Util.requireNonNull((SkullMeta) skull.getItemMeta(), "SkullMeta");
+        sMeta.setOwningPlayer(offlinePlayer);
         sMeta.setDisplayName(displayName);
         skull.setItemMeta(sMeta);
-        return Optional.of(skull);
+        return skull;
     }
 
     @Override
     public void initialize()
     {
-        headMap = TimedCache.<UUID, Optional<ItemStack>>builder()
+        headMap = TimedCache.<UUID, ItemStack>builder()
                             .duration(Duration.ofMinutes(config.headCacheTimeout()))
                             .cleanup(Duration.ofMinutes(Math.max(1, config.headCacheTimeout())))
                             .softReference(true).build();
@@ -106,8 +99,7 @@ public final class HeadManager extends Restartable
     @Override
     public void shutDown()
     {
-        if (headMap != null)
-            headMap.shutDown();
-        headMap = null;
+        headMap.shutDown();
+        headMap = TimedCache.emptyCache();
     }
 }
