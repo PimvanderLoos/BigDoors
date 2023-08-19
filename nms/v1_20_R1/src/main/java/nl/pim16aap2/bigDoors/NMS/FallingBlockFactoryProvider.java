ead16468f171d37ccd02321575e50437e0f3d936
Pim van der Loos <pimvanderloos@gmail.com>
2023-08-19 13:31:31 +0200

    Use ByteBuddy to support all builds of MC1.20.1
    - A recent change in CraftBukkit providing a final implementation in the CraftEntity class for the previously-abstract method Entity#getType. We fixed this by simply removing this method so users could have a working version of this plugin on new builds of Spigot/Paper/etc. However, this meant that the plugin no longer worked on older builds, as our CustomCraftFallingBlock class (a subclass of CraftEntity) did not provide an implementation for the getType method. Using ByteBuddy, we generate a new subclass of our CustomCraftFallingBlock class with the added getType method for builds that require it.

--- nms/v1_20_R1/src/main/java/nl/pim16aap2/bigDoors/NMS/FallingBlockFactoryProvider.java
+++ nms/v1_20_R1/src/main/java/nl/pim16aap2/bigDoors/NMS/FallingBlockFactoryProvider.java
@@ -5,15 +5,31 @@ import org.bukkit.craftbukkit.v1_20_R1.entity.CraftEntity;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+/**
+ * Provides a {@link FallingBlockFactory} for version 1.20.R1.
+ * <p>
+ * The exact type of {@link FallingBlockFactory} returned depends on the specific build of Spigot/Paper/etc.
+ * <p>
+ * Use {@link #getFactory()} to get a new {@link FallingBlockFactory} instance.
+ */
 public class FallingBlockFactoryProvider
 {
+    /**
+     * Creates a new {@link FallingBlockFactory} instance for version 1.20.R1 of Minecraft for the current server build.
+     *
+     * @return A new {@link FallingBlockFactory} instance.
+     */
     public static FallingBlockFactory getFactory()
     {
+        return new FallingBlockFactory_V1_20_R1(getCraftEntitySupplier());
+    }
+
+    private static FallingBlockFactory_V1_20_R1.CustomCraftFallingBlockFactory getCraftEntitySupplier()
+    {
         if (methodGetTypeIsFinal())
-            return new FallingBlockFactory_V1_20_R1();
+            return CustomCraftFallingBlock_V1_20_R1::new;
         else
-            throw new UnsupportedOperationException(
-                "This version of Spigot/Paper/etc is not supported! Please update your server to a more recent build!");
+            return new CustomCraftEntityGenerator().getCraftEntitySupplier();
     }
 
     /**
--- nms/v1_20_R1/src/main/java/nl/pim16aap2/bigDoors/NMS/FallingBlockFactory_V1_20_R1.java
+++ nms/v1_20_R1/src/main/java/nl/pim16aap2/bigDoors/NMS/FallingBlockFactory_V1_20_R1.java
@@ -8,17 +8,27 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_20_R1.CraftServer;
 import org.bukkit.craftbukkit.v1_20_R1.CraftWorld;
 
 class FallingBlockFactory_V1_20_R1 implements FallingBlockFactory
 {
+    private final CustomCraftFallingBlockFactory customCraftEntityConstructor;
+
+    FallingBlockFactory_V1_20_R1(CustomCraftFallingBlockFactory customCraftEntityConstructor)
+    {
+        this.customCraftEntityConstructor = customCraftEntityConstructor;
+    }
+
     @Override
     public CustomCraftFallingBlock fallingBlockFactory(Location loc, NMSBlock block, byte matData, Material mat)
     {
-        IBlockData blockData = ((NMSBlock_V1_20_R1) block).getMyBlockData();
-        CustomEntityFallingBlock_V1_20_R1 fBlockNMS
+        final IBlockData blockData = ((NMSBlock_V1_20_R1) block).getMyBlockData();
+        final CustomEntityFallingBlock_V1_20_R1 fBlockNMS
             = new CustomEntityFallingBlock_V1_20_R1(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), blockData);
-        CustomCraftFallingBlock_V1_20_R1 entity = new CustomCraftFallingBlock_V1_20_R1(Bukkit.getServer(), fBlockNMS);
+
+        final CustomCraftFallingBlock_V1_20_R1 entity =
+            customCraftEntityConstructor.newCraftEntity((CraftServer) Bukkit.getServer(), fBlockNMS);
         entity.setCustomName("BigDoorsEntity");
         entity.setCustomNameVisible(false);
         return entity;
@@ -30,5 +40,10 @@ class FallingBlockFactory_V1_20_R1 implements FallingBlockFactory
         final Info blockInfo =
             Info.a((BlockBase) ((CraftWorld) world).getHandle().a_(new BlockPosition(x, y, z)).b());
         return new NMSBlock_V1_20_R1(world, x, y, z, blockInfo);
+    }
+
+    interface CustomCraftFallingBlockFactory
+    {
+        CustomCraftFallingBlock_V1_20_R1 newCraftEntity(CraftServer server, CustomEntityFallingBlock_V1_20_R1 entity);
     }
 }
